ec9c349164721dd40eeb69530c940c20abe24f84
Le Nguyen <helloworld2xxx@yahoo.com>
2017-12-07 18:39:23 +0700

    Chia se voi Git

---
+++ QuanLySach/Models/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace QuanLySach.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public ApplicationDbContext()
+            : base("QuanLySachConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+    }
+}
